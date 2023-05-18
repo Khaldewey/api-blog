354eaa337654262a9c8119321ca562bfa7602278
Israel <israel_alves77@hotmail.com>
2023-05-17 21:37:30 -0300

    implementação de principais camadas

---
+++ src/main/java/com/israelita/isblog/isblog/IsblogApplication.java
@@ -0,0 +1,13 @@
+package com.israelita.isblog.isblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelita/isblog/isblog/IsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelita.isblog.isblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
